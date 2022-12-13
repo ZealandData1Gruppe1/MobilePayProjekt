792ba8f7337a33e4a68b43f79f649f44b85db169
SercanAtlantis <120471786+SercanAtlantis@users.noreply.github.com>
2022-12-13 14:13:36 +0100

    hej

--- MPcontroller.java
+++ MPcontroller.java
@@ -1,7 +1,5 @@
 public class MPcontroller {
     public static void main(String[] args) {
-        System.out.println("Hej Kevin");
-        System.out.println("Hej Sercan");
-        System.out.println("Hej Emil");
+
     }
 }
