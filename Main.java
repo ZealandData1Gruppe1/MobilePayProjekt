b0be6f08ea17037d2d4351fe4750c76d6f209544
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2022-12-16 14:00:43 +0100

    Menu tilføjet til program

--- Main.java
+++ Main.java
@@ -4,31 +4,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         DBSQL dbsql = new DBSQL();
-        ArrayList<Transaktion> test = new ArrayList<Transaktion>();
-        /*
-        Person p1 =new Person("Kevin","1234","1234567","11111111");
-        Person p2 =new Person("Emil","0000","2345678","22222222");
-        Person p3 =new Person("Jakob","1234","12346156","33333333");
-        Person p4 =new Person("Sercan","1234","653465435","44444444");
-
-        dbsql.opretPerson(p1);
-        dbsql.opretPerson(p2);
-        dbsql.opretPerson(p3);
-        dbsql.opretPerson(p4);
-        */
-
-        Person p5 = new Person();
-        Person p6 = new Person();
-        Person p7 = new Person();
-        Virksomhed v1;
-        p5 = dbsql.hentPerson("11111111");
-        p6 = dbsql.hentPerson("22222222");
-        p7 = dbsql.hentPerson("33333333");
-        v1 = new Virksomhed("Shell","1234","123456","20202020");
-        v1 =dbsql.hentVirksomhed("123456");
-
-        Transaktion t5 = new Transaktion(p5,v1,10.0,new Date(),"Test11");
-        dbsql.opretTransaktion(t5);
 
     }
 }
--- Menu.java
+++ Menu.java
@@ -1,25 +1,23 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Menu {
-    private DBSQL dbsql = new DBSQL();
+
     private Bruger aktivBruger;
+    private DBSQL dbSql = new DBSQL();
+
 
     //Constructor
-
-    public Menu(DBSQL dbsql) {
-        this.dbsql = dbsql;
-    }
-    public Menu()
-    {
-
-    }
 
     //Metoder
     public void godkendTransaktion(Transaktion t)
     {
-
+        getDBSql().opretTransaktion(t);
+        getDBSql().withdrawMoney(t.getAfsender(),t.getAmount());
+        getDBSql().depositMoney(t.getModtager(),t.getAmount());
     }
 
     public void opretBruger()
@@ -30,8 +28,6 @@ public class Menu {
         int brugerInput = input.nextInt();
         if (brugerInput == 1){
             Person nyPerson = new Person();
-            System.out.println("Indtast bankkonto");
-            nyPerson.addBankKonto(nyPerson,input.next());//Kig på BankKonto addBankKonto, hvordan tilføjer vi bruger
             System.out.println("Indtast navn");
             nyPerson.setNavn(input.next());
             System.out.println("Indtast den ønskede kode");
@@ -40,52 +36,81 @@ public class Menu {
             nyPerson.setCpr(input.next());
             System.out.println("Indtast telefonnummer: ");
             nyPerson.setTelefonNR(input.next());
-            //Opdatere databasen
+            dbSql.opretPerson(nyPerson);
             System.out.println("Du er nu oprettet!");
 
         } else if (brugerInput == 2)
         {
             Virksomhed nyVirksomhed = new Virksomhed();
-            System.out.println("Indtast bankkonto");
-            nyVirksomhed.addBankKonto(nyVirksomhed,input.next());//Kig på BankKonto addBankKonto, hvordan tilføjer vi bruger
             System.out.println("Indtast navn");
             nyVirksomhed.setNavn(input.next());
             System.out.println("Indsæt dit unikke 5 cifret nummer");// Virksomheden skal selv finde på et nummer
             nyVirksomhed.setVirksomhedsNR(input.next());
             System.out.println("Indsæt Virksomhedens CVR nummer: ");
             nyVirksomhed.setCvr(input.next());
-            //Opdatere databasen
+            dbSql.opretVirksomhed(nyVirksomhed);
             System.out.println("Du er nu oprettet!");
         }
         else System.out.println("Det er ikke en muliged.");
     }
 
+    public void opretTransaktion(Transaktion t){
+
+            dbSql.opretTransaktion(t);
+    }
+
     public void sendPenge(Bruger afsender, Bruger modtager, double amount, String kommentar, Date dato)
     {
-
         double afsenderBalance = afsender.getB().getBalance();
         if (afsenderBalance < amount)
             System.out.println("Der er ikke nok penge på kontoen");
-        else {
-            dbsql.withdrawMoney(afsender, amount);
-            dbsql.depositMoney(modtager, amount);
-            Transaktion nyTransaktion = new Transaktion(afsender, modtager, amount, dato, kommentar);
-            dbsql.opretTransaktion(nyTransaktion);
-        }
+        else
+            afsenderBalance-= amount;
+        //Send opdater databasen.
+
+        double modtagersBalance = modtager.getB().getBalance();
+        modtagersBalance += amount;
+        //Opdatere databasen
+
+        Transaktion nyTransaktion = new Transaktion(afsender,modtager,amount,dato,kommentar);
+        //Opdater databasen
     }
-    public void visAnmodninger(Bruger b)
-    {
+    public void visAnmodninger(Bruger modtager) {
         DBSQL dbsqlAnmod = new DBSQL();
-        ArrayList<Transaktion> anmodninger = dbsqlAnmod.hentAnmodninger(b.getBrugerID());
+        ArrayList<Transaktion> anmodninger = dbsqlAnmod.hentAnmodninger(modtager.getBrugerID());
         //databasen returner et array
-        for (Transaktion t : anmodninger){
+        for (Transaktion t : anmodninger) {
             System.out.println(t);
         }
     }
-    public void godkendAnmodning(Transaktion t)
-    {
-        
+    public void visHistorik(Bruger bruger){
+        //ArrayList<Transaktion> historik = getDBSql().hentHistorikPerson(bruger.getBrugerID());
+        //for (Transaktion transaktion : historik){
+            //System.out.println(transaktion);
+
+            //sorter så den nyeste står øverst
+        }
+
+
+    public void godkendAnmodning(Transaktion anmodning) {
+
+        System.out.println(anmodning);
+        System.out.println("Tast 1 for at godkende eller tast 2 for at annullere");
+        Scanner input = new Scanner(System.in);
+        int brugerInput = input.nextInt();
+        if(brugerInput == 1) {
+            System.out.println("Anmodning godkendt");
+            getDBSql().opretTransaktion(anmodning);
+            getDBSql().withdrawMoney(anmodning.getAfsender(),anmodning.getAmount());
+            getDBSql().depositMoney(anmodning.getModtager(),anmodning.getAmount());
+        }
+        else if (brugerInput == 2){
+            System.out.println("Anmodning annulleret");
+            //getDBSql().sletAnmodning(transaktionID);
+        }
     }
+
+
 
     public void logIn()
     {
@@ -104,11 +129,13 @@ public class Menu {
             Person loginPerson = new Person();
             loginPerson.setTelefonNR(telefonnummer);
             loginPerson.setKode(kode);
-            //Send forespørgelse til database
-            boolean forespørgelse = false;
-            if (forespørgelse){
-                Person loggetInd = new Person();
-                // loggetInd = oplysninger fra databasen
+            Person databasePerson  = new Person();
+            databasePerson.hentBruger(telefonnummer);
+            if(loginPerson.getTelefonNR().equals(databasePerson.getTelefonNR())
+                    && loginPerson.getKode().equals(databasePerson.getKode()))
+            {
+                Bruger loggetInd = aktivBruger;
+                //loggedInd = oplysninger fra database
                 System.out.println("Du er nu logget ind");
             }
             else System.out.println("Forkert telefonnummer eller kode");
@@ -121,20 +148,22 @@ public class Menu {
             Virksomhed loginVirksomhed = new Virksomhed();
             loginVirksomhed.setVirksomhedsNR(virksomhednummer);
             loginVirksomhed.setKode(kode);
-            //Send forespørgelse til database
-            boolean forespørgelse = false;
-            if (forespørgelse){
-                Virksomhed loggetInd = new Virksomhed();
+            Virksomhed databaseVirksomhed = new Virksomhed();
+            databaseVirksomhed.hentBruger(virksomhednummer);
+            if(loginVirksomhed.getVirksomhedsNR().equals(databaseVirksomhed.getVirksomhedsNR())
+                    && loginVirksomhed.getKode().equals(databaseVirksomhed.getKode()))
+            {
+                Bruger loggetInd = aktivBruger;
                 // loggetInd = oplysninger fra databasen
                 System.out.println("Du er nu logget ind");
             }
-            else System.out.println("Forkert telefonnummer eller kode");
+            else System.out.println("Forkert virksomhed nummer eller kode");
         }
     }
 
     public void logUd()
     {
-       //skal lukke programmet
+        //skal lukke programmet
     }
     public Bruger getAktivBruger() {
         return aktivBruger;
@@ -143,4 +172,19 @@ public class Menu {
     public void setAktivBruger(Bruger aktivBruger) {
         this.aktivBruger = aktivBruger;
     }
+
+    public DBSQL getDBSql(){
+        return dbSql;
+    }
+
+    public boolean tjekBalance(Bruger bruger, double amount){
+        bruger.getB().getBalance();
+        if( bruger.getB().getBalance() >= amount){
+            return true;
+        }
+        else{System.out.println("Beløbet overstiger din nuværende saldo");
+            return false;
+        }
+    }
+
 }
